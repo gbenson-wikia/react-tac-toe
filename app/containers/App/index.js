52bbbafa6ae22500f274c581cdfd3f95de2d54e9
Geoffrey Benson <xaroth@gmail.com>
2017-11-26 17:42:02 -0800

    Remove the 404 page

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,18 +12,12 @@
  */
 
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 export default function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
+    <Route component={HomePage} />
   );
 }
